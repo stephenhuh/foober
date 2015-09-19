c6bb6ae810ab6239ac7c585644d0f473cc2ff333
zujko <zujko@csh.rit.edu>
2015-09-19 02:35:34 -0400

    Updated GetYelp job to take dynamic query params

--- app/src/main/java/superduper/foober/Job/GetYelp.java
+++ app/src/main/java/superduper/foober/Job/GetYelp.java
@@ -12,21 +12,36 @@ import superduper.foober.Event.YelpEvent;
  * Created by anhbui on 9/19/15.
  */
 public class GetYelp extends Job {
-    public GetYelp() {
+    private double latitude;
+    private double longitude;
+    private int radiusFilter;
+    private int limit;
+    private String term;
+
+    public GetYelp(double latitude, double longitude, int radiusFilter, int limit, String term) {
         super(new Params(1));
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radiusFilter = radiusFilter;
+        this.limit = limit;
+        this.term = term;
     }
 
     @Override
-    public void onAdded() {
-
-    }
+    public void onAdded() {}
 
     @Override
     public void onRun() throws Throwable {
         YelpAPI yelpApi = new YelpAPI("C7jpYv-BIy90FZNFVKb0sA", "Y4Q5KP0Ix4lDjAtLuZFTwoOBDzU",
                 "gAipPmG2nAR80B1Rb1BKBdLe2hnWY1i0", "gklfMFQUqn0HKTZfpxPl_BpzWKc");
         QueryParams queryParams = QueryParams.builder()
+                .setLatitude(latitude)
+                .setLongitude(longitude)
+                .setRadiusFilter(radiusFilter)
+                .setLimit(limit)
+                .setTerm(term)
                 .build();
+
         EventBus.getDefault().post(new YelpEvent(yelpApi.queryAPI(yelpApi, queryParams)));
     }
 
